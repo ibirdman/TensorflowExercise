cd1cc6f70e75613f29b0606bec7e3b2bcea4ba4b
Stefan Gao <stefan.gao@gmail.com>
2018-11-18 21:57:29 +0800

    a little change

--- image_test1.py
+++ image_test1.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import matplotlib.image as img
 import matplotlib.pyplot as plot
-myfile = "likegeeks.png"
+myfile = "data/likegeeks.png"
 myimage = img.imread(myfile)
 slice = tf.placeholder("int32",[None,None,3])
 cropped = tf.slice(myimage,[10,0,0],[516,-1,-1])
